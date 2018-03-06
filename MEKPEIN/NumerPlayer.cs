c35d5d9f65e7b38b88cb3606e1518d63d2d035ca
Stefan Kelemen <photosynthese@gmx.at>
2018-03-06 18:02:21 +0100

    created weird class to give data an action and just activate it...

---
+++ MEKPEIN/NumerPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace MEKPEIN
+{
+    /// <summary>
+    /// This class can store values and shift them
+    /// </summary>
+    public class NumerPlayer
+    {
+        public NumerPlayer(Action<NumerPlayer> a)
+        {
+            A = a;
+        }
+
+        public int N = 0;
+        public Action<NumerPlayer> A;
+        public void Kick() { A.Invoke(this); }
+
+        public Point Point { get; set; }
+    }
+}
